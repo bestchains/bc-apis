0aca16feb1c08eaff4c671ada2b14832c838d2aa
dayuy <973860441@qq.com>
2023-03-16 14:45:40 +0800

    feat: add field profileJson of channel(#46)

--- src/channel/channel.module.ts
+++ src/channel/channel.module.ts
@@ -3,10 +3,11 @@ import { ChannelService } from './channel.service';
 import { ChannelResolver } from './channel.resolver';
 import { ChannelLoader } from './channel.loader';
 import { EpolicyModule } from 'src/epolicy/epolicy.module';
+import { ConfigmapModule } from 'src/configmap/configmap.module';
 
 @Module({
   providers: [ChannelService, ChannelResolver, ChannelLoader],
   exports: [ChannelLoader, ChannelService],
-  imports: [forwardRef(() => EpolicyModule)],
+  imports: [forwardRef(() => EpolicyModule), ConfigmapModule],
 })
 export class ChannelModule {}
