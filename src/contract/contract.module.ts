9bfe060832217dfa9b3a6c0611304c1014bcfec8
dayuy <973860441@qq.com>
2023-04-21 15:16:11 +0800

    feat: import official contracts into network(#83)

--- src/contract/contract.module.ts
+++ src/contract/contract.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ContractService } from './contract.service';
 import { ContractResolver } from './contract.resolver';
 import { MinioModule } from 'src/minio/minio.module';
+import { ChaincodebuildModule } from 'src/chaincodebuild/chaincodebuild.module';
 
 @Module({
   providers: [ContractService, ContractResolver],
-  imports: [MinioModule],
+  imports: [MinioModule, ChaincodebuildModule],
 })
 export class ContractModule {}
--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -1,17 +1,36 @@
-import { HttpException, Injectable, Logger } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import {
   BESTCHAINS_CONTRACTS_BUCKET,
   BESTCHAINS_CONTRACTS_GIT,
+  CustomException,
+  MINIO_BUCKET_NAME,
 } from 'src/common/utils';
 import { RequestOptions } from 'urllib';
 import * as urllib from 'urllib';
+import * as fs from 'node:fs';
 import { Contract } from './models/contract.model';
 import { MinioService } from 'src/minio/minio.service';
 import { Lang } from './models/Lang.enum';
+import { JwtAuth } from 'src/types';
+import { ChaincodebuildService } from 'src/chaincodebuild/chaincodebuild.service';
+import imageConfig from 'src/config/image.config';
 
 @Injectable()
 export class ContractService {
-  constructor(private readonly minioService: MinioService) {}
+  constructor(
+    private readonly minioService: MinioService,
+    private readonly ccbService: ChaincodebuildService,
+    @Inject(imageConfig.KEY)
+    private imgConfig: ConfigType<typeof imageConfig>,
+  ) {}
 
   logger = new Logger('ContractService');
 
@@ -72,5 +91,110 @@ export class ContractService {
       this.logger.error('getContracts', error);
       return null;
     }
+  }
+
+  async importContract(
+    auth: JwtAuth,
+    name: string,
+    network: string,
+  ): Promise<boolean> {
+    // 0. 获取详情
+    const contracts = await this.getContracts(Lang.en);
+    const contract = contracts.find((d) => d.name === name);
+    if (!contract) {
+      throw new CustomException(
+        'FORBIDDEN_CONTRACT_NOT_EXIST',
+        'the contract does not exist',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    // 1. 校验该网络中此合约是否已存在
+    const { version, description, package: pkg } = contract;
+    const ccbs = await this.ccbService.getChaincodebuilds(auth, {
+      id: name,
+      network,
+      version,
+    });
+    if (ccbs?.length > 0) {
+      return true;
+    }
+    // 2. 复制官方合约到MinIO bestchains中，并修改其Dockerfile
+    const timestampName = await this.copyContractFilesToMinIO();
+    await this.modifyContractDockerfileToMinIO(timestampName, pkg);
+
+    // 2. 创建合约
+    await this.ccbService.createChaincodebuildCR(auth, timestampName, {
+      network,
+      version,
+      description,
+      displayName: name,
+    });
+    return true;
+  }
+
+  // 替换MinIO中bestchains-contracts的Dockerfile文件
+  async modifyContractDockerfileToMinIO(
+    dirName: string,
+    contractPkg: string,
+  ): Promise<boolean> {
+    const objectName = `${dirName}/Dockerfile`;
+    const tempFile = '/tmp/Dockerfile';
+
+    await this.minioService.fgetObject(MINIO_BUCKET_NAME, objectName, tempFile);
+    const content = fs.readFileSync(tempFile, 'utf-8');
+    const newContent = content
+      .replace('hyperledgerk8s', this.imgConfig.namespace)
+      .replace('examples/basic', contractPkg)
+      .replace('FROM golang:', `FROM ${this.imgConfig.namespace}/golang:`);
+    fs.writeFileSync(tempFile, newContent);
+    await this.minioService.fputObject(MINIO_BUCKET_NAME, objectName, tempFile);
+    return true;
+  }
+
+  // 复制官方合约到MinIO bestchains中
+  async copyContractFilesToMinIO(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      try {
+        const stream = this.minioService.listObjects(
+          BESTCHAINS_CONTRACTS_BUCKET,
+          'bestchains-contracts/',
+        );
+        const objs = [];
+        stream.on('data', (obj) => {
+          objs.push(obj);
+        });
+        stream.on('end', async () => {
+          let isFirst = false;
+          let objectName: string;
+          for (const obj of objs) {
+            const fileName = obj.name;
+            if (!isFirst) {
+              objectName = `${fileName?.split('/')[0]}-${Date.now()}`;
+              isFirst = true;
+            }
+            const hashFileName = fileName.replace(
+              /^([^/]+)(\/.*)$/,
+              `${objectName}$2`,
+            );
+            await this.minioService.copyObject(
+              MINIO_BUCKET_NAME,
+              hashFileName,
+              `${BESTCHAINS_CONTRACTS_BUCKET}/${fileName}`,
+            );
+          }
+          this.logger.debug('Copy files is done');
+          resolve(objectName);
+        });
+        stream.on('error', (err) => {
+          this.logger.error('minio error: ', err);
+          throw new InternalServerErrorException(
+            'Failed to copy files from minIO.',
+          );
+        });
+      } catch (error) {
+        this.logger.error(error);
+        reject(error);
+      }
+    });
   }
 }
